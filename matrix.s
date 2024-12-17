227000dc688e9d5a323951dfaaa6b7f08e78868e
yishuihan <1070401527@qq.com>
2024-12-17 20:23:57 +0800

    first push

---
+++ matrix.s
@@ -0,0 +1,10 @@
+
+	AREA	program, CODE, READONLY
+matrix_mul
+	EXPORT	matrix_mul
+
+    ; Please write your code below that will implement:
+    ;       int matrix_mul(Matrix *results, const Matrix *source1, const Matrix *source2);
+	
+	BX		LR
+	END
